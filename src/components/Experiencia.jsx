a35f0839e0b22232d107d7d2d5e0c69ffcc5d40d
tonypeanut <tonypeanut93@gmail.com>
2024-03-27 16:26:19 -0700

    avances miercoles 27 de marzo 2024

--- src/components/Experiencia.jsx
+++ src/components/Experiencia.jsx
@@ -1,9 +1,26 @@
-const Experiencia = () => {
-  return (
-    <>
-        <div>Experiencia</div>
-    </>
-  )
+
+
+const Experiencia = ({datosExperiencia}) => {
+
+    return (
+      <>
+        <div className=" container flex w-full justify-around">
+          {datosExperiencia.map(experiencia => (
+            <div className="container bg-gray-200 w-5/12 ml5 mt-5 mb-5 border rounded">
+                <p><strong>Puesto:</strong> {experiencia.Puesto}</p>
+                <p><strong>Fecha de Inicio:</strong> {experiencia.FechaInicio}</p>
+                <p><strong>Fecha de Fin:</strong> {experiencia.FechaFin}</p>
+                <p><strong>Empresa:</strong> {experiencia.Empresa}</p>
+                <ul>
+                  {experiencia.Funciones.map((funcion, i) => (
+                    <li key={i}>{funcion}</li>
+                  ))}
+                </ul>
+            </div>
+          ))}
+        </div>
+      </>
+    )
 }
 
 export default Experiencia
--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -5,14 +5,18 @@ import Experiencia from "../components/Experiencia";
 import Proyectos from "../components/Proyectos";
 import Contacto from "../components/Contacto";
 
+import datos from '../data/datos.json'
+
 const Inicio = () => {
+  const { datosCursos, datosExperiencia, datosSkills, datosSoftskills} = datos
+
   return (
     <>
         <div className=" flex flex-col items-center">
             <Presentacion/>
             <Acerca/>
             <Skills/>
-            <Experiencia/>
+            <Experiencia datosExperiencia={datosExperiencia}/>
             <Proyectos/>
             <Contacto/>
 

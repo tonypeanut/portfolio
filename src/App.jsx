6c6f0c01ba8a821bb4f457437cc42a9a740c7826
tonypeanut <tonypeanut93@gmail.com>
2024-04-12 16:07:00 -0700

    se usa glob para importar imagenes

--- src/App.jsx
+++ src/App.jsx
@@ -8,18 +8,23 @@ import TodosProyectos from './pages/TodosProyectos';
 import { ContextProvider } from './context/ContextProvider';
 
 function App() {
-  return (
-      <BrowserRouter>
-            <ContextProvider>
-                <Routes>
-                        <Route path='/' element = {<Layout/>}>
-                            <Route index element ={<Inicio/>}/>
-                            <Route path='proyectos' element= {<TodosProyectos/>}/>
-                        </Route>
-                </Routes>
-            </ContextProvider>
-      </BrowserRouter>
-  )
-}
+    const iconFiles = import.meta.glob('./assets/icons/*.svg');
+    const imageFiles = import.meta.glob('./assets/images/*.png');
+    console.log(iconFiles);
+    console.log(imageFiles);
+
+    return (
+        <BrowserRouter>
+                <ContextProvider>
+                    <Routes>
+                            <Route path='/' element = {<Layout/>}>
+                                <Route index element ={<Inicio/>}/>
+                                <Route path='proyectos' element= {<TodosProyectos/>}/>
+                            </Route>
+                    </Routes>
+                </ContextProvider>
+        </BrowserRouter>
+    )
+    }
 
 export default App

ce77d4d5dfb6808782c61baff6318b689c1e03e1
tonypeanut <tonypeanut93@gmail.com>
2024-07-05 16:21:56 -0700

    Actualiza el CV

--- src/components/RedesSociales.jsx
+++ src/components/RedesSociales.jsx
@@ -13,7 +13,7 @@ const RedesSociales = () => {
       <div className=' w-full flex justify-center p-1' >
           <Link to="https://linkedin.com/in/antonio-valdez-84b4549b/" target="_blank" rel="noopener noreferrer"><img className=" p-0.5 ml-3 mr3 hover:shadow hover:shadow-cyan-400 hover:bg-cyan-400" src={linkedinSVG} alt="linkedin" width={`${size}px`}/> </Link>
           <Link to="https://github.com/tonypeanut/" target="_blank" rel="noopener noreferrer"><img className=" p-0.5 ml-3 mr3 hover:shadow hover:shadow-cyan-400 hover:bg-cyan-400" src={githubSVG} alt="github" width={`${size}px`}/></Link>
-          <a href='https://drive.google.com/file/d/1j7GnE5hkB-bxB9ZcB4YsgPxQnZ1ESpxt/view?usp=drive_link' target='_blank'><img className=" p-0.5 ml-3 mr3 hover:shadow hover:shadow-cyan-400 hover:bg-cyan-400" src={curriculumVitaeSVG} alt="curriculum vitae" width={`${size}px`}/></a>
+          <a href='https://drive.google.com/file/d/1Cyh_AsLfJMZgsv9h904bBQmTcEop_55L/view?usp=drive_link' target='_blank'><img className=" p-0.5 ml-3 mr3 hover:shadow hover:shadow-cyan-400 hover:bg-cyan-400" src={curriculumVitaeSVG} alt="curriculum vitae" width={`${size}px`}/></a>
       </div>
     </>
   )

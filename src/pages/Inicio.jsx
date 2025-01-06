e687a681d1ec6737fbbb5fbf4179ec2b746e2cb6
tonypeanut <155868763+tonypeanut@users.noreply.github.com>
2025-01-06 00:36:06 -0700

    Pruebas de producción 7

--- src/pages/Inicio.jsx
+++ src/pages/Inicio.jsx
@@ -69,10 +69,10 @@ const Inicio = () => {
       <Acerca />
       {/*
       <Skills datosSkills={datosSkills} datosSoftskills={datosSoftskills} />
-      */}
+      
       
       <Experiencia datosExperiencia={datosExperiencia} />
-      {/*
+     
       <Proyectos limit={6} datosProyectos={datosProyectos} />
       */}
       <Contacto />

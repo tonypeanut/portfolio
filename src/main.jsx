e033b77adeea9819e11f55f787d479d765034bc8
tonypeanut <155868763+tonypeanut@users.noreply.github.com>
2025-01-06 17:58:19 -0700

    Ajuste 17

--- src/main.jsx
+++ src/main.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
-import './languages/i18n.js'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+import i18n from './languages/i18n'; // Asegúrate de que este archivo inicializa correctamente i18n
+import { I18nextProvider } from 'react-i18next';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+    <I18nextProvider i18n={i18n}>
+      <App />
+    </I18nextProvider>
+  </React.StrictMode>
+);

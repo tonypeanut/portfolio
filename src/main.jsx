13d59004eb8156e72eaaa28bf870cb59f342170c
tonypeanut <155868763+tonypeanut@users.noreply.github.com>
2025-01-06 18:05:05 -0700

    Ajuste 18

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
@@ -8,7 +8,9 @@ import { I18nextProvider } from 'react-i18next';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <I18nextProvider i18n={i18n}>
-      <App />
+      <Suspense fallback={<div>Loading translations...</div>}>
+        <App />
+      </Suspense>
     </I18nextProvider>
   </React.StrictMode>
 );
